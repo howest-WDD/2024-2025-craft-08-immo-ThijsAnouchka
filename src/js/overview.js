cc5fe96ec5c41cb5ee2368da99a4c83bc99997cc
xAnouchka <anouchka.thijs@student.howest.be>
2024-11-22 11:56:39 +0100

    feat: show houses on map

--- src/js/overview.js
+++ src/js/overview.js
@@ -3,9 +3,8 @@
 import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
-let mymap
-const showMap = function(){
-
+let mymap;
+const showMap = function () {
   mapboxgl.accessToken = import.meta.env.VITE_SECRET_KEY_MAPBOX; //keep it secret in GitHub!
   mymap = new mapboxgl.Map({
     container: 'map-id', // container ID
@@ -14,16 +13,19 @@ const showMap = function(){
     center: [4.35735646652748, 50.845687598666984], // starting position [lng, lat]
     zoom: 7, // starting zoom
   });
+};
 
-}
+const showMarkers = function () {
+  const arrLocations = document.querySelectorAll('.js-immo-location');
+  for (const location of arrLocations) {
+    const lat = location.dataset.lat;
+    const lng = location.dataset.long;
+    const marker = new mapboxgl.Marker().setLngLat([lng, lat]).addTo(mymap);
+  }
+};
 
-const showMarkers = function(){
-
-
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-	console.log('DOM geladen');
-	showMap();
-	showMarkers();
+document.addEventListener('DOMContentLoaded', function () {
+  console.log('DOM geladen');
+  showMap();
+  showMarkers();
 });
